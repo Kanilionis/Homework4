00d418d0579c537ac186e41867db98ed79bc3654
Kanilionis <kanilionis@gmail.com>
2020-09-30 22:55:09 -0600

    consolidated questions into object, called to object with for loop in functions, unhid endScreen, added screenshots to README

--- README.md
+++ README.md
@@ -1,19 +1,9 @@
 # Homework4
 quiz
 
-# 04 Web APIs: Code Quiz
+## Quiz Navigation
+- 
 
-As you proceed in your career as a web developer, you will probably be asked to complete a coding assessment, which is typically a combination of multiple-choice questions and interactive challenges. Build a timed code quiz with multiple-choice questions. This app will run in the browser and feature dynamically updated HTML and CSS powered by your JavaScript code. It will also feature a clean and polished user interface and be responsive, ensuring that it adapts to multiple screen sizes.
-
-## User Story
-
-```
-AS A coding bootcamp student
-I WANT to take a timed quiz on JavaScript fundamentals that stores high scores
-SO THAT I can gauge my progress compared to my peers
-```
-
-## Acceptance Criteria
 
  ### Pseudo Code
  
@@ -35,75 +25,29 @@ keep track of how many questions have been answered and where the timer is...cle
 
 create functions for each step
 
+# Start of Quiz
 GIVEN I am taking a code quiz
 WHEN I click the start button
-**done** THEN a timer starts 
-    THEN I am presented with a question (how does this happen? Use setTimeout(initialFunction, 2000))
-WHEN I answer a question correctly (how will the code know that it is the correct answer?)
-    THEN I am presented with another question
-WHEN I answer a question incorrectly
-    THEN time is subtracted from the clock (global variables)
-    THEN I am presented with another question (function)
-WHEN all questions are answered or the timer reaches 0
-    THEN the game is over (function)
-WHEN the game is over
-    THEN I can save my initials and score
-```
-
-The following animation demonstrates the application functionality:
-
-![code quiz](./Assets/04-web-apis-homework-demo.gif)
-
-### Review
-
-You are required to submit the following for review:
-
-* The URL of the functional, deployed application.
-
-* The URL of the GitHub repository. Give the repository a unique name and include a README describing the project.
-
-- - -
-© 2019 Trilogy Education Services, a 2U, Inc. brand. All Rights Reserved.
+    <img src="startScreen.jpg>
+### THEN a timer starts 
+- set timer for 100, counting down 1 second at a time
+### THEN I am presented with a question (how does this happen? Use setTimeout(initialFunction, 2000)). WHEN I answer a question correctly (how will the code know that it is the correct answer?). THEN I am presented with another question
+- added all questions/answer choices/correct answer in an object, then referenced object in for loop when function was called
+### WHEN I answer a question incorrectly, THEN time is subtracted from the clock (global variables)
+- lose 10 seconds every time the correct answer button is not clicked
+### WHEN all questions are answered or the timer reaches 0, THEN the game is over (function)
+- added an alert when last question answered with score, moved to endScreen to log results and username
+    <img src="endOfGamerAlert.jpg";
+### WHEN the game is over, THEN I can save my initials and score
+- I need to get more familiar with the localStorage function as I could only figure out how to log the score
+    <img src="userResults.jpg">
+### Reset Button
+- reset button starts clock over and returns to blank screen, but I need to figure out how to restart the quiz
 
 
 
-## questions:
-Inside which HTML element do we put the JavaScript?
-    c - <script>
-    <scripting>
-    <js>
-    <javascript>
-Where is the correct plae to insert a JavaScript?
-    <head>
-    <body>
-    c - both <head> and <body>
-    <footer>
-How do you create a function in JavaScript?
-    c - function myFunction()
-    function: myFunction()
-    function = myFunction()
-    function function()
-JavaScript is the same as Java.
-    true
-    c - false
-Which operator is used to assign a value to a variable?
-    c - =
-    -
-    x
-    *
-Which of the following function of String object returns the character at the specified index?
-    c - charAt()
-    charCodeAt()
-    concat()
-    indexOf() 
-Which of the below is NOT a JavaScript Data type?
-    Number
-    String
-    Boolean
-    c - Counter
-What method would you use to turn a string into an array?
-    c - .string()
-    .push()
-    .concat()
-    .cook()
+
+
+
+
 
--- script.js
+++ script.js
@@ -1,222 +1,195 @@
-// add main element
-var mainEl = document.createElement("div", "main");
-    document.body.appendChild(mainEl);
 
-// where are we going to store questions, results, etc.
-var quizContainer = document.createElement("div", "quiz-container");
-    mainEl.appendChild(quizContainer);
-var resultsContainer = document.createElement("div", "results");
-    mainEl.appendChild(resultsContainer);
-
-
-var clock = document.createElement("div", "clock");
-    clock.setAttribute("id", "timer");
-    document.body.appendChild(clock);
-var minutesLeft = 10;
-var currentTime = Date.parse(new Date());
-// console.log(currentTime);
-var timerEnd = new Date(currentTime + minutesLeft*60*1000);
-// console.log(timerEnd);
-
-
-// create buttons
-var startBtn = document.createElement("button", "start-button");
-    startBtn.textContent = "Start Quiz";
-    startBtn.addEventListener("click", function(){
-        startQuiz();
-    });
-    quizContainer.appendChild(startBtn);
-
-var submitButton = document.createElement("button", "submit");
-    submitButton.textContent = "Submit Answer";
-    submitButton.addEventListener("click", function(e){
-        e.preventDefault;
-        console.log("submit")
-    });
-    mainEl.appendChild(submitButton);
-
-var resetButton = document.createElement("button", "reset");
-    resetButton.textContent = "Reset Score"
-    resetButton.addEventListener("click", function(e){
-        e.preventDefault;
-        console.log("clear-score");
-    }); 
-    resultsContainer.appendChild(resetButton);
-
-var resultsBtn = document.createElement("button", "results-button");
-    resultsBtn.textContent = "Get Final Results";
-    resultsBtn.addEventListener("click", function(e){
-        e.preventDefault;
-        console.log("show results")
-    });
-    resultsContainer.appendChild(resultsBtn);
-
-// create questions
-var question1 = document.createElement("ul", "question1");
-    question1.textContent = "Inside which HTML element do we put the JavaScript?";
-    quizContainer.appendChild(question1);
-    var ulTag = document.createElement("ul");
-    question1.appendChild(ulTag)
-    var answers1 = ["<script>", "<scripting>", "<js>", "<javascript>"];
-    // "<script>"
-    for( var i=0; i<answers1.length; i++){
-      var choices1 = document.createElement("button");
-      choices1.textContent = answers1[i] + "";
-      choices1.addEventListener("click", function(){});
-      ulTag.appendChild(choices1);}
-    
-    
-var question2 = document.createElement("ul", "question2");
-    question2.textContent = "Where is the correct place to insert a JavaScript?";
-    quizContainer.appendChild(question2);
-    var ulTag = document.createElement("ul");
-    question2.appendChild(ulTag)
-    var answers2 = ["<head>", "<body>", "both <head> and <body>", "<footer>"];
-    // "both <head> and <body>"
-      for( var i=0; i<answers1.length; i++){
-        var choices2 = document.createElement("button");
-        choices2.textContent = answers2[i] + "";
-        choices2.addEventListener("click", function(){});
-        ulTag.appendChild(choices2);}
-          
-var question3 = document.createElement("ul", "question3");
-    question3.textContent = "How do you create a function in JavaScript?";
-    quizContainer.appendChild(question3);
-    var ulTag = document.createElement("ul");
-    question3.appendChild(ulTag)
-    var answers3 = ["function: myFunction()", "function myFunction()", "function = myFunction()", "function function()",];
-    // "function: myFunction()"
-      for( var i=0; i<answers3.length; i++){
-        var choices3 = document.createElement("button");
-        choices3.textContent = answers3[i] + "";
-        choices3.addEventListener("click", function(){
-          console.log(answers3[i])
-        });
-        ulTag.appendChild(choices3);}
-
-var question4 = document.createElement("ul", "question4");
-    question4.textContent = "True or False: JavaScript is the same as Java?";
-    quizContainer.appendChild(question4);
-    var ulTag = document.createElement("ul");
-    question4.appendChild(ulTag)
-    var answers4 = ["true", "false"];
-    // "false"
-      for( var i=0; i<answers4.length; i++){
-        var choices4 = document.createElement("button");
-        choices4.textContent = answers4[i] + "";
-        choices4.addEventListener("click", function(){
-          console.log(answers4[i])
-        });
-        ulTag.appendChild(choices4);}
-      
-var question5 = document.createElement("ul", "question5");
-    question5.textContent = "Which operator is used to assign a value to a variable?";
-    quizContainer.appendChild(question5);
-    var ulTag = document.createElement("ul");
-    question5.appendChild(ulTag)
-    var answers5 = ["=", "-", "x", "*"];
-    // "="
-      for( var i=0; i<answers5.length; i++){
-        var choices5 = document.createElement("button");
-        choices5.textContent = answers5[i] + "";
-        choices5.addEventListener("click", function(){
-          console.log(answers5[i])
-        });
-        ulTag.appendChild(choices5);}
-        
-var question6 = document.createElement("ul", "question6");
-    question6.textContent = "Which of the following function of String object returns the character at the specified index?";
-    quizContainer.appendChild(question6);
-    var ulTag = document.createElement("ul");
-    question6.appendChild(ulTag)
-    var answers6 = ["concat()", "charCodeAt()", "charAt()", "indexOf()",];
-    // "charAt()"
-      for( var i=0; i<answers6.length; i++){
-        var choices6 = document.createElement("button");
-        choices6.textContent = answers6[i] + "";
-        choices6.addEventListener("click", function(){
-          console.log(answers6[i])
-        });
-        ulTag.appendChild(choices6);}
-      
-var question7 = document.createElement("ul", "question7");
-    question7.textContent = "Which of the below is NOT a JavaScript Data type?";
-    quizContainer.appendChild(question7);
-    var ulTag = document.createElement("ul");
-    question7.appendChild(ulTag)
-    var answers7 = ["Number", "String", "Boolean", "Counter",];
-    // "Counter"
-      for( var i=0; i<answers7.length; i++){
-        var choices7 = document.createElement("button");
-        choices7.textContent = answers7[i] + "";
-        choices7.addEventListener("click", function(){
-          console.log(answers7[i])
-        });
-        ulTag.appendChild(choices7);}
-
-var question8 = document.createElement("ul", "question8");
-    question8.textContent = "What method would you use to turn a string into an array?";
-    quizContainer.appendChild(question8);
-    var ulTag = document.createElement("ul");
-    question8.appendChild(ulTag)
-    var answers8 = [".push()", ".concat()", ".cook()", ".split()"];
-    // ".split()"
-      for( var i=0; i<answers8.length; i++){
-        var choices8 = document.createElement("button");
-        choices8.textContent = answers8[i] + "";
-        choices8.addEventListener("click", function(){
-          console.log(answers8[i])
-        });
-        ulTag.appendChild(choices8);}
+var time = 100;
+var timerId = document.getElementById("timerId");
+var start = document.getElementById("start-screen");
+var startBtn = document.getElementById("start");
+var questions = document.getElementById("questions");
+var quizQuestions = document.getElementById("quizQuestion");
+var choicesEl = document.getElementById("choices");
+var resultsBtn = document.getElementById("results");
+var endScreen = document.getElementById("endScreen");
+var userName = document.getElementById("userName");
+var resetBtn = document.getElementById("reset");
+var userResults = document.getElementById("userResults");
+// variable we are changing to change questions (1 to 2, 2 to 3, etc)
+var index = 0;
+var questionsArray = [
+  {
+    questionTitle: "Inside which HTML element do we put the JavaScript?",
+    choices: ["<script>", "<scripting>", "<js>", "<javascript>"],
+    correctAnswer: "<script>",
+  },
+  {
+    questionTitle: "Where is the correct place to insert a JavaScript?",
+    choices: ["<head>", "<body>", "both <head> and <body> are correct", "<footer>"],
+    correctAnswer: "both <head> and <body> are correct",
+  },
+  {
+    questionTitle: "How do you create a function in JavaScript?",
+    choices: ["function: myFunction()", "function myFunction()", "function = myFunction()", "function function()"],
+    correctAnswer: "function: myFunction()",
+  },
+  {
+    questionTitle: "True or False: JavaScript is the same as Java?",
+    choices: ["true", "false"],
+    correctAnswer: "false",
+  },
+  {
+    questionTitle: "Which of the following function of String object returns the character at the specified index?",
+    choices: ["concat()", "charCodeAt()", "charAt()", "indexOf()"],
+    correctAnswer: "charAt()",
+  },
+  {
+    questionTitle: "Which operator is used to assign a value to a variable?",
+    choices: ["=", "-", "x", "*"],
+    correctAnswer: "=",
+  },
+  {
+    questionTitle: "Which of the below is NOT a JavaScript Data type?",
+    choices: ["Number", "String", "Boolean", "Counter"],
+    correctAnswer: "Counter",
+  },
+  {
+    questionTitle: "What method would you use to turn a string into an array?",
+    choices: [".push()", ".concat()", ".cook()", ".split()"],
+    correctAnswer: ".split()",
+  },
+];
 
 
+function timer(){
+  time--
+  timerId.textContent = time;
+  if(time <= 0){
+  time = 0;
+  endQuiz();
+  }
+  }
+  var counter = setInterval(timer, 1000);
 function startQuiz(){
-    console.log("start quiz");
+  counter.textContent = time;
+  start.setAttribute("class", "hidden");
+  questions.removeAttribute("class");
+  // want time to run each second (1000 miliseconds)
+  
+  // call next function
+getQuestion()
+}
+
+function getQuestion(){
+  console.log("test")
+  var currentQuestion = questionsArray[index];
+  quizQuestions.textContent = currentQuestion.questionTitle;
+  // clears out div to make empty string
+  choices.innerHTML = ""
+  currentQuestion.choices.forEach(function(choice,i){
+    console.log(choice,i);
+    var answerChoice = document.createElement("button");
+    answerChoice.setAttribute("value", choice);
+    answerChoice.textContent = choice;
+    // attach onclick here
+    answerChoice.onclick = correctAnswer;
+    choicesEl.appendChild(answerChoice);
+  })
+}
+
+function correctAnswer(){
+  if (this.value !== questionsArray[index].correctAnswer){
+    time -= 10;
+    timerId.textContent = time;
+    if (time <= 0){
+      // time will never go below 0
+      time = 0;
+      endQuiz();
+    }}
+    index++;
+    if(index === questionsArray.length){
+      endQuiz();
+    }else{
+      getQuestion();
     }
-
- function nextQuestion(){
-
- }   
-
- function keepScore(){
-// increment and decrement
- }
-
-function showResults(){
-  // results button
+    }
+//e.target - tells us what was clicked on - can add to local storage
+function endQuiz(){
+ clearInterval(counter);
+ questions.setAttribute("class", "hidden");
+//  timerId.setAttribute("class", "hidden"); 
+//  endScreen.removeAttribute("class");
+ logResults();
+ endScreen.removeAttribute("class");
+  alert("You finished the quiz with " + time + " seconds remaining! Your final score is " + time + "!")
 }
 
-function clearScore(){
-   // reset button
-}
 
+function logResults(){
+  // var logUser = localStorage.getItem("#userName");
+  var logScoreSpan = document.getElementById("logScore");
+  localStorage.getItem("time");
+  var userNameSpan = document.getElementById("logUser").value;
+  localStorage.getItem("userName");
+  // localStorage.getItem("logUser");
+  resultsBtn.addEventListener("click", function(e){
+    e.preventDefault();
+    userResults.removeAttribute("class");
+        // userNameSpan.textContent = userName;
+        logScoreSpan.textContent = time;
+      }
+  )}
+
+resetGame();
+
+function resetGame(){
+  resetBtn.addEventListener("click", function(){
+    endScreen.setAttribute("class", "hidden");
+    userResults.setAttribute("class", "hidden");
+    timerId.textContent= 100;})
+    start.removeAttribute("class");
+
+  }
+
+  localStorage.setItem("logUser", logUser);
+  localStorage.setItem("time", logScore);
+
+
+// put at bottom later
+startBtn.onclick = startQuiz
+
+
+// var clock = document.createElement("div", "clock");
+//     clock.setAttribute("id", "timer");
+//     document.body.appendChild(clock);
+// var minutesLeft = 1.5;
+// var currentTime = Date.parse(new Date());
+// // console.log(currentTime);
+// var timerEnd = new Date(currentTime + minutesLeft*60*1000);
+// // console.log(timerEnd);
 
 
 // create timer
-function countdown(endTime){
-        var t = Date.parse(endTime) - Date.parse(new Date());
-        var seconds = Math.floor( (t/1000) % 60 );
-        var minutes = Math.floor( (t/1000/60) % 60 );
-        return {'minutes':minutes, 'seconds':seconds};
-}
-function runClock(endTime){
-    var timeInterval = setInterval(updateClock, 1000);
-    var clock = document.getElementById("timer");
-    function updateClock(){
-        var t = countdown(endTime);
-        clock.textContent = "0" + t.minutes + ":" + t.seconds;
-        if(t.seconds === 0 && t.minutes === 0){
-            clock.textContent = "0" + t.minutes + ":0" + t.seconds;
-            clearInterval(timeInterval);
-        } else if(t.seconds <10){
-            clock.textContent = "0" + t.minutes + ":0" + t.seconds;
-        } else if(t.seconds === 0 && t.minutes === 0){
-            clearInterval(timeInterval);
-            alert("YOU HAVE RUN OUT OF TIME!");
-        }
-        }
-    };
-    runClock(timerEnd);
+// function countdown(endTime){
+//         var t = Date.parse(endTime) - Date.parse(new Date());
+//         var seconds = Math.floor( (t/1000) % 60 );
+//         var minutes = Math.floor( (t/1000/60) % 60 );
+//         return {'minutes':minutes, 'seconds':seconds};
+// }
+// function runClock(endTime){
+//     var timeInterval = setInterval(updateClock, 1000);
+//     var clock = document.getElementById("timer");
+//     function updateClock(){
+//         var t = countdown(endTime);
+//         clock.textContent = "0" + t.minutes + ":" + t.seconds;
+//         if(t.seconds === 0 && t.minutes === 0){
+//             clock.textContent = "0" + t.minutes + ":0" + t.seconds;
+//             clearInterval(timeInterval);
+//         } else if(t.seconds <10){
+//             clock.textContent = "0" + t.minutes + ":0" + t.seconds;
+//         // } else if(t.seconds === 0 && t.minutes === 0){
+//         //     clearInterval(timeInterval);
+//         //     alert("YOU HAVE RUN OUT OF TIME!");
+//         }
+//         }
+//     };
+//     runClock(timerEnd);
 
 
 
